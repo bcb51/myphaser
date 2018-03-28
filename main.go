c986b3952d4a13743ee27d2f8062e6d3d60e677e
willy <979899660@qq.com>
2018-03-28 12:33:54 +0800

    init

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+import (
+	"net/http"
+)
+
+func main(){
+	 http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./static"))))
+	 http.ListenAndServe(":80",nil)
+	 
+
+
+
+}
